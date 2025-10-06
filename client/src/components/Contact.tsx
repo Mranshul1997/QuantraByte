b2e3ec716f805146b1f7d0631784abe5219b0a8b
Anshul <139344452+Mranshul1997@users.noreply.github.com>
2025-10-06 15:12:12 +0530

    add

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -76,7 +76,7 @@ const Contact = () => {
 
     setIsSubmitting(true);
     try {
-      const res = await fetch("https://quantrabyte.com/contact", {
+      const res = await fetch("/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
